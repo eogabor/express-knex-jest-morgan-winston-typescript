c44db00ea9d594894426641881aab7ab22b9d0bd
eogabor <geabor5@gmail.com>
2022-07-27 11:30:48 +0200

    small fixes

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,3 +1,1 @@
-import express from "express";
-
-export const routes = express.Router();
+export const controllers = {};
---
+++ tests/initial.ts
@@ -0,0 +1,7 @@
+export const initialTestSuite = () => {
+  describe("initial", () => {
+    test("initial test", () => {
+      expect(1).toBe(1);
+    });
+  });
+};
