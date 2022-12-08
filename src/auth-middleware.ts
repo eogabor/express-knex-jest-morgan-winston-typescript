ad64538355c73ecfdd6e10e11bf483c16ccfae3c
eogabor <geabor5@gmail.com>
2022-12-08 16:18:07 +0100

    axios,moment added auth middleware added .env added to gitignore

---
+++ src/auth-middleware.ts
@@ -0,0 +1,31 @@
+import axios from "axios";
+import express from "express";
+
+const authUrl = "http://192.168.0.239:3800";
+
+export async function authMiddleware(
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) {
+  const token = req.headers["x-access-token"];
+
+  if (!token) {
+    return res.status(401).json({ message: "No token provided!" });
+  }
+
+  return axios
+    .post(`${authUrl}/token/verify-token`, {
+      accessToken: token,
+    })
+    .then((response) => {
+      if (response.status === 200) {
+        next();
+      } else {
+        return res.status(401).send({ message: "Token verification failed!" });
+      }
+    })
+    .catch((error) => {
+      return res.status(401).send({ message: "Token verification failed!" });
+    });
+}
